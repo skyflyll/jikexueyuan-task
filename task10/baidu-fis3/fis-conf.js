b524cdb36911fc39033638c120a5f63160ae7a19
linlang <270901715@qq.com>
2017-03-21 20:27:54 +0800

    commit task10 baidu-fis3

---
+++ task10/baidu-fis3/fis-conf.js
@@ -0,0 +1,36 @@
+
+// 加 md5
+fis.match('*.{js,css,png}', {
+  useHash: true
+});
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+});
+
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true
+});
+
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+
+fis.match('*.html', {
+  //invoke fis-optimizer-html-minifier 插件
+  optimizer: fis.plugin('html-minifier')
+});
